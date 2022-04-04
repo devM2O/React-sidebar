d2baa161409649127ea9b2792c376253c78e2909
minnmawoo <minmawoo.ucsm@gmail.com>
2022-04-05 01:05:52 +0630

    final

--- src/Sidebar/index.js
+++ src/Sidebar/index.js
@@ -13,8 +13,10 @@ import { NavLink } from "react-router-dom";
 
 const Container = styled.div`
   position: fixed;
+
   .active {
     border-right: 4px solid var(--white);
+
     img {
       filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(93deg)
         brightness(103%) contrast(103%);
@@ -30,10 +32,13 @@ const Button = styled.button`
   border-radius: 50%;
   margin: 0.5rem 0 0 0.5rem;
   cursor: pointer;
+
   display: flex;
   justify-content: center;
   align-items: center;
+
   position: relative;
+
   &::before,
   &::after {
     content: "";
@@ -43,10 +48,12 @@ const Button = styled.button`
     position: absolute;
     transition: all 0.3s ease;
   }
+
   &::before {
     top: ${(props) => (props.clicked ? "1.5" : "1rem")};
     transform: ${(props) => (props.clicked ? "rotate(135deg)" : "rotate(0)")};
   }
+
   &::after {
     top: ${(props) => (props.clicked ? "1.2" : "1.5rem")};
     transform: ${(props) => (props.clicked ? "rotate(-135deg)" : "rotate(0)")};
@@ -60,15 +67,18 @@ const SidebarContainer = styled.div`
   margin-top: 1rem;
   border-radius: 0 30px 30px 0;
   padding: 1rem 0;
+
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
+
   position: relative;
 `;
 
 const Logo = styled.div`
-  width: 2rem;
+  width: 4rem;
+
   img {
     width: 100%;
     height: auto;
@@ -82,10 +92,13 @@ const SlickBar = styled.ul`
   flex-direction: column;
   align-items: center;
   background-color: var(--black);
+
   padding: 2rem 0;
+
   position: absolute;
   top: 6rem;
   left: 0;
+
   width: ${(props) => (props.clicked ? "12rem" : "3.5rem")};
   transition: all 0.5s ease;
   border-radius: 0 30px 30px 0;
@@ -97,15 +110,19 @@ const Item = styled(NavLink)`
   width: 100%;
   padding: 1rem 0;
   cursor: pointer;
+
   display: flex;
   padding-left: 1rem;
+
   &:hover {
     border-right: 4px solid var(--white);
+
     img {
       filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(93deg)
         brightness(103%) contrast(103%);
     }
   }
+
   img {
     width: 1.2rem;
     height: auto;
@@ -122,23 +139,29 @@ const Text = styled.span`
 `;
 
 const Profile = styled.div`
-  width: ${(props) => (props.clicked ? "14rem" : "3rem")};
+  width: ${(props) => (props.clicked ? "15rem" : "3rem")};
   height: 3rem;
+
   padding: 0.5rem 1rem;
   /* border: 2px solid var(--white); */
   border-radius: 20px;
+
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-left: ${(props) => (props.clicked ? "9rem" : "0")};
+  margin-left: ${(props) => (props.clicked ? "11rem" : "0")};
+
   background-color: var(--black);
   color: var(--white);
+
   transition: all 0.3s ease;
+
   img {
     width: 2.5rem;
     height: 2.5rem;
     border-radius: 50%;
     cursor: pointer;
+
     &:hover {
       border: 2px solid var(--grey);
       padding: 2px;
@@ -154,17 +177,21 @@ const Details = styled.div`
 
 const Name = styled.div`
   padding: 0 1.5rem;
+
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
   h4 {
     display: inline-block;
   }
+
   a {
     font-size: 0.8rem;
     text-decoration: none;
     color: var(--grey);
+
     &:hover {
       text-decoration: underline;
     }
@@ -176,6 +203,7 @@ const Logout = styled.button`
   width: 2rem;
   height: 2rem;
   background-color: transparent;
+
   img {
     width: 100%;
     height: auto;
@@ -258,12 +286,16 @@ const Sidebar = () => {
           />
           <Details clicked={profileClick}>
             <Name>
-              <h4>Jhon&nbsp;Doe</h4>
+              <h4>Min&nbsp;Maw&nbsp;Oo</h4>
               <a href="/#">view&nbsp;profile</a>
             </Name>
 
-            <Logout>
-              <img src={PowerOff} alt="logout" />
+            <Logout clicked={profileClick}>
+              <img
+                onClick={() => handleProfileClick()}
+                src={PowerOff}
+                alt="logout"
+              />
             </Logout>
           </Details>
         </Profile>
